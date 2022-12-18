fc5ca7936e028764069cb0ef6f2338f4df275a47
Jasmine <tanghocle456@gmail.com>
2022-12-17 20:15:54 -0500

    Add documentation, fix get_new_list() bug

--- src/chip_liar_game.py
+++ src/chip_liar_game.py
@@ -1,6 +1,9 @@
 import math
 class CL:
     def B(q: int, j: int) -> float:
+        """
+            The function that computes B(q,j) based on the paper
+        """
         sum = 0.0
         for i in range(0,j+1):
             sum += math.comb(q,i)
@@ -20,7 +23,15 @@ class CL:
     # CL : Chip-Liar
     # Jcond: Jasmine win condition
     def mult_CL_Jcond(q = -1, inputList = None):
+        """
+        Computes the weight of the chip liar game
 
+        mult: stands for (x0, x1,..., xk) game
+
+        CL : Chip-Liar
+
+        Jcond: Jasmine win condition
+        """
         # If the user hasn't specified any input value
         if(q < 0):
             q, inputList = CL.get_input(q, inputList)
@@ -32,15 +43,49 @@ class CL:
 
     
     def get_new_list(isTrue, queryList, inputList):
+        """
+        Get the new, remaining set based on Jasmine's answer of Yes or No for Lucy's query
+
+        **Args**
+
+        `isTrue`: stands for Jasmine's answer: True for Yes, False for No
+
+        `queryList`: stands for the Lucy's query set S
+
+        `ls`: stands for the complement set S' 
+
+        ** Mechanics **
+
+        Suppose Jasmine answers Yes, then we have to move S' to the left. What this really means is if S' has $y_i$ chips at position $i$ and the remaining set
+        has $x_i$ chips at position $i$, we have to subtract $y_i$ from $x_i$ and add $y_i$ to $x_{i-1}$ after the subtraction
+
+        Let ls = remaining_set - queryList (element wise subtraction)
+        
+        The act of subtracting $y_i$ from $x_i$ is subtracting ls from the remaining set
+
+        Thus, remaining_set - ls = remaining_set - (ls - queryList) = queryList
+
+        Then the act of adding $y_i$ to $x_{i-1]$ can be seen as adding the left-shifted S' to the subtraction result
+
+        Thus, remaing_set - ls + left_shifted_S' = S + left_shifted_S'
+    
+        
+        Suppose Jasmine answer No, then we have to move S to the left. Moving S to the left is the same thing as subtracting S from the original list, then 
+        add the left-shifted S to the subtraction result
+
+        Then remaing_set - S + left_shift_S = S' + left_shift_S
+        """
         # queryList stands for the set S
         # ls stands for the set S'
         ls = [inputList[i] - queryList[i] for i in range(len(inputList))]
         if(isTrue):
-            queryList = queryList[1:] + [0]
-            return [ls[i] + queryList[i] for i in range(len(inputList))]
-        else:
+            # Answering Yes, we shift the set S' to the left, and add it to the set S
             ls = ls[1:] + [0]
             return [queryList[i] + ls[i] for i in range(len(inputList))]
+        else:
+            # Answering No: we shift the set S to the left, and add it to the set S'
+            queryList = queryList[1:] + [0]
+            return [ls[i] + queryList[i] for i in range(len(inputList))]
 
 
 
